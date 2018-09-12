e23cc01e17f8e49a392c1bec6490524f6eb701b3
Addison Kimball <addison@akimball.com>
2018-09-12 03:55:28 -0500

    lots of progress, converting from block to flex

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 const electron = require('electron')
+const filesystem = require('fs')
+const path = require('path');
+const {dialog} = require('electron')
 
 const {app, BrowserWindow} = require('electron')
-  
-  function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({
-      width: 1100,
-      height: 650,
-      frame: false
-    })
-  
-    // and load the index.html of the app.
-    win.loadURL('https://web.groupme.com', {userAgent: ''})
+let win;
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1100,
+    height: 650,
+	titleBarStyle: "hidden",
+	show: false,
+  })
 
-    //win.webContents.openDevTools()
-  }
+	//win.loadFile('app/index.html')
+	win.loadURL('https://web.groupme.com', {userAgent: ""})
+	var customCSS = filesystem.readFileSync("app/css/index.css", "utf8")
+	var contents = win.webContents
+	contents.on("dom-ready", () => {
+		contents.insertCSS(customCSS)
+		win.show()
+		
+	})
+}
+
+function showAlert() {
+
+	dialog.showMessageBox({message: 'hi!'})
+
+
+}
+
   
-  app.on('ready', createWindow)
+app.on('ready', createWindow)
+
+
+
+  
+
+
+
 
