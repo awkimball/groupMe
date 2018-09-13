b83631005791767d1e0b404ab8afedaa69ccd4de
Addison Kimball <addison@akimball.com>
2018-09-12 21:04:12 -0500

    redone most CSS, full flexbox layout now

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
 const electron = require('electron')
 const filesystem = require('fs')
-const path = require('path');
 const {dialog} = require('electron')
+const {webFrame} = require('electron');
+const path = require('path')
 
 const {app, BrowserWindow} = require('electron')
 let win;
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({
-    width: 1100,
-    height: 650,
-	titleBarStyle: "hidden",
-	show: false,
-  })
+  	// Create the browser window.
+	win = new BrowserWindow({
+		width: 1100,
+		height: 650,
+		titleBarStyle: "hidden",
+		show: false,
+	})
+
+	// dialog.showMessageBox({message: __dirname})
 
 	//win.loadFile('app/index.html')
 	win.loadURL('https://web.groupme.com', {userAgent: ""})
-	var customCSS = filesystem.readFileSync("app/css/index.css", "utf8")
+	var customCSS = filesystem.readFileSync(path.join(__dirname, "app/css/index.css"), "utf8")
 	var contents = win.webContents
 	contents.on("dom-ready", () => {
 		contents.insertCSS(customCSS)
 		win.show()
 		
 	})
+
+	
 }
 
 function showAlert() {
